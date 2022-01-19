c582c33373b66b6b3feac755743e0ecedd5fa57b
edobasky <edore7@gmail.com>
2022-01-19 22:56:23 +0100

    initial commit

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Contracts;
+using LoggerService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        // configuring cors policy for allowing acces to our app
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyOrigin()
+                .AllowAnyHeader());
+            });
+        }
+
+        // configure IIS integration to help with deployment to IIS
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options => { });
+
+
+        public static void ConfigureLoggerService(this IServiceCollection services) =>
+            services.AddScoped<ILoggerManager, LoggerManager>();
+    }
+}
